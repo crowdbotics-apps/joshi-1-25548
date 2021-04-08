3993bfe24c60813bcc21b4e872c3aa9b9f8e7fa9
Crowdbotics <team@crowdbotics.com>
2021-04-08 12:14:29 +0000

    Update components

---
+++ screens/BlankScreen0222819/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <View>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({ View_1: {}, Button_3: {} })
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
